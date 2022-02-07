41a96114063dc39a28dae5ed0eb50ff811844310
Tomas Novitskas <nov35@live.ru>
2022-02-07 18:14:39 +0200

    test

---
+++ timer.h
@@ -0,0 +1,19 @@
+#ifndef TIMER_H
+#define TIMER_H
+#include <chrono>
+
+class Timer {
+    using cl_t = std::chrono::high_resolution_clock;
+    using sec_t = std::chrono::duration<double, std::ratio<1> >;
+
+    std::chrono::time_point<cl_t> start;
+public:
+    Timer() : start(cl_t::now()) {}
+    void reset(){
+	start = cl_t::now();
+    }
+    double get() const{
+    	return std::chrono::duration_cast<sec_t>(cl_t::now() - start).count();
+    }
+};
+#endif
